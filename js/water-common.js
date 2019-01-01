66a5d33796db57ea4ac08e092a6459a39f99c2b5
Maksym Kolesnyk <brhumaan@gmail.com>
2019-01-01 17:35:11 +0200

    img optimazed; water bg upd

--- js/water-common.js
+++ js/water-common.js
@@ -1,21 +1,31 @@
 // Underwater world by Maksim K.
 // brhumaan@gmail.com
 
+// UI
+var preloaderStatus = true;
+
+
 // Variables
 var waterColors =[
-    '#B1E5EF',
-    '#C9E7ED',
-    '#C7EAE5',
-    '#C5E5D5',
-    '#D4EFF4',
-    '#D1EDD0',
-    '#C9CCE5',
-    '#1E5799',
-    '#2256AA',
-    '#2845B7',
+    // '#B1E5EF',
+    // '#C9E7ED',
+    // '#C7EAE5',
+    // '#C5E5D5',
+    // '#D4EFF4',
+    // '#D1EDD0',
+    // '#C9CCE5',
+    // '#1E5799',
+    // '#2256AA',
+    // '#2845B7',
     '#2B52C6',
     '#2F58D6',
     '#37A0FC',
+    '#092e34',
+    '#092634',
+    '#092034',
+    '#091234',
+    '#093429',
+    '#092a34',
 ]
 
 // Fish
@@ -73,12 +83,27 @@ var seaWeedArr = [
 
 
 // First app start
+preloader();
 nextWorld();
+$(".next-world-button").click(function(){
+    nextWorld();
+})
+
 
 
 function nextWorld(){
-    clearApp()
-    worldGenetarors()
+    console.log("nextworld started")
+    // If preloader status true
+    if(preloaderStatus){
+        preloader();
+        setTimeout(() => {
+            clearApp()
+            worldGenetarors()
+        }, 400);
+    } else {
+        clearApp()
+        worldGenetarors()
+    }
 }
 
 
@@ -87,9 +112,7 @@ function worldGenetarors(){
     createFish();
     createBubbles();
     createBottom();
-    // createContentObjects()
 }
-
 
 
 function createWater() {
@@ -110,8 +133,8 @@ function createWater() {
             + convertHex(waterColor2, randomNumber(10, 20)) + ' 0% , '
             + convertHex(waterColor2, randomNumber(20, 30)) + ' 20% , '
             + convertHex(waterColor2, randomNumber(50, 70)) + ' 50% , '
-            + convertHex(waterColor2, randomNumber(20, 30)) + ' 80% , '
-            + convertHex(waterColor2, randomNumber(10, 20)) + '100%',
+            + convertHex(waterColor2, randomNumber(40, 50)) + ' 80% , '
+            + convertHex(waterColor2, randomNumber(30, 40)) + '100%',
         'opacity': '1'
     })
 
@@ -120,26 +143,27 @@ function createWater() {
         "background": 'linear-gradient(to bottom,' 
             + convertHex(waterColor3, randomNumber(10, 20)) + ' 0% , '
             + convertHex(waterColor3, randomNumber(30, 50)) + ' 20% , '
-            + convertHex(waterColor3, randomNumber(50, 70)) + ' 50% , '
-            + convertHex(waterColor3, randomNumber(40, 50)) + ' 80% , '
-            + convertHex(waterColor3, randomNumber(20, 30)) + '100%',
+            + convertHex(waterColor3, randomNumber(60, 70)) + ' 50% , '
+            + convertHex(waterColor3, randomNumber(50, 60)) + ' 80% , '
+            + convertHex(waterColor3, randomNumber(40, 50)) + '100%',
         'opacity': '1',
     })
     var waterColor4 = waterColors[randomNumber(0, waterColors.length - 1)];
     $(".water-4").css({
         "background": 'linear-gradient(to bottom,' 
             + convertHex(waterColor4, randomNumber(10, 20)) + ' 0% , ' 
-            + convertHex(waterColor4, randomNumber(30, 50)) + ' 20% , '
-            + convertHex(waterColor4, randomNumber(50, 70)) + ' 50% , '
-            + convertHex(waterColor4, randomNumber(30, 50)) + ' 80% , '
-            + convertHex(waterColor4, randomNumber(20, 30)) + '100%',
+            + convertHex(waterColor4, randomNumber(50, 50)) + ' 20% , '
+            + convertHex(waterColor4, randomNumber(60, 70)) + ' 50% , '
+            + convertHex(waterColor4, randomNumber(40, 50)) + ' 80% , '
+            + convertHex(waterColor4, randomNumber(30, 40)) + '100%',
         'opacity': '1',
     })
+    var waterColor5 = waterColors[randomNumber(0, waterColors.length - 1)];
     $(".water-5").css({
         "background": 'linear-gradient(to bottom,' 
-            + convertHex(waterColors[randomNumber(0, waterColors.length - 1)], randomNumber(1, 5)) + ' 0% , ' 
-            + convertHex(waterColors[randomNumber(0, waterColors.length - 1)], randomNumber(5, 10)) + ' 50% , '
-            + convertHex(waterColors[randomNumber(0, waterColors.length - 1)], randomNumber(1, 5)) + '100%',
+            + convertHex(waterColor5, randomNumber(2, 4)) + ' 0% , ' 
+            + convertHex(waterColor5, randomNumber(2, 4)) + ' 50% , '
+            + convertHex(waterColor5, randomNumber(2, 4)) + '100%',
         'opacity': '1',
     })
 }
@@ -164,17 +188,17 @@ function createFish(){
 
         // .content-1
         if (i == 1) { 
-            fishSize = fishArr[i][1] / 6; 
+            fishSize = fishArr[i][1] / 5; 
             howManyFishToLayer = fishNumber;
         }
         // .content-2
         else if (i == 2) { 
-            fishSize = fishArr[i][1] / 4;
+            fishSize = fishArr[i][1] / 2;
             howManyFishToLayer = fishNumber / randomNumber(1,5); 
         }
         // .content-3
         else if (i == 3) { 
-            fishSize = fishArr[i][1];
+            fishSize = fishArr[i][1] * 1.3;
             howManyFishToLayer = fishNumber / randomNumber(5,8); 
         }
         // .content-4
@@ -275,6 +299,9 @@ function createBubbles() {
 }
 
 
+
+
+
 function createBottom() {
     // Go to every content-wrap
     for (var i = 1; i < 5; i++) {
@@ -285,26 +312,22 @@ function createBottom() {
         
         // .content-1
         if (i == 1) {
-            console.log(1);
             seaWeedSize = 1;
             weedNumber = seaWeedNumbers * 4;
         }
         // .content-2
         else if (i == 2) {
             seaWeedSize = 2;
-            console.log(2);
             weedNumber = seaWeedNumbers * 3;
         }
         // .content-3
         else if (i == 3) {
             seaWeedSize = 3;
-            console.log(3);
             weedNumber = seaWeedNumbers * 1.5;
         }
         // .content-4
         else if (i == 4) {
             seaWeedSize = 8;
-            console.log(4);
             weedNumber = seaWeedNumbers / randomNumber(4, 8);
         }
 
@@ -346,8 +369,27 @@ function clearApp() {
 } 
 
 
-
 // UI
+// FIrst preloader launch
+
+// First preloader launch
+function preloader() {
+    $(".preloader").fadeIn(200);
+    
+    setTimeout(() => {
+        $(".preloader").fadeOut(400);
+    }, 1000);
+}
+
+$("#ui-preloader-checkbox").click(function(){
+    if (document.getElementById('ui-preloader-checkbox').checked){
+        preloaderStatus = true;
+    } else {
+        preloaderStatus = false;
+    }
+})
+
+
 // Start by enter
 $(document).keypress(function (e) {
     if (e.which == 13) {
@@ -355,12 +397,21 @@ $(document).keypress(function (e) {
     }
 });
 
-// Toogle settings by space
-// $(document).keypress(function (e) {
-//     if (e.which == 32) {
-//         $(".ui-button-toggle").click();
-//     }
-// });
+// Settings modal
+$(".settings-button, .settings-modal .close").click(function (e) {
+    $(".settings-modal").toggleClass('on');
+})
+
+
+// Author modal
+$(".about-author-button, .about-author-modal .close").click(function (e) {
+    var el = ".element";
+
+    $(".about-author-modal").toggleClass('on');
+})
+
+
+
 
 
 // Random number
